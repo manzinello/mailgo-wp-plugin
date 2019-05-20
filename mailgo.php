3b17290dd8f87a1c123a42426b78048475fecedb
Matteo Manzinello <matteo@manzinello.com>
2019-05-20 19:44:23 +0200

    mailgo 0.4.0, updated READMEs

--- mailgo.php
+++ mailgo.php
@@ -3,7 +3,7 @@
  * mailgo, a different mailto
  *
  * @link              https://mailgo.js.org
- * @since             0.3.3
+ * @since             0.4.0
  * @package           mailgo
  *
  * @wordpress-plugin
@@ -11,7 +11,7 @@
  * Plugin URI:        https://mailgo.js.org
  * Text Domain:       mailgo
  * Description:       a different mailto: more possibilities, less spam
- * Version:           0.3.3
+ * Version:           0.4.0
  * Author:            Matteo Manzinello
  * Author URI:        https://matteomanzinello.com
  * License:           GPL-2.0+
@@ -25,7 +25,7 @@ if (!defined('WPINC')) {
 
 function mailgo_adding_scripts()
 {
-    wp_register_script('mailgo_script', plugins_url('dist/mailgo.min.js', __FILE__), array(), '0.3.3', true);
+    wp_register_script('mailgo_script', plugins_url('dist/mailgo.min.js', __FILE__), array(), '0.4.0', true);
     wp_enqueue_script('mailgo_script');
 }
 
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Tags: mailto, mail
 Requires at least: 4.8
 Tested up to: 5.2
 Requires PHP: 5.2.4
-Stable tag: 0.3.3
+Stable tag: 0.4.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -28,6 +28,9 @@ Instead of the classic behaviour of mailto links, mailgo will show a modal with 
 1. mailgo modal
 
 == Changelog ==
+
+= 0.4.0 =
+mailgo 0.4.0 (links opening in new tabs, keyboard commands)
 
 = 0.3.3 =
 mailgo 0.3.3
